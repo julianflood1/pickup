4c09be983d4738f5bcfa0dae6ff596c300527ac2
jf <julianflood@gmail.com>
2017-08-28 12:01:24 -0700

    fix select

--- src/app/select/select.component.ts
+++ src/app/select/select.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Game } from '../game.model';
+import { Router } from '@angular/router';
+import { GameService } from '../game.service';
+import { FirebaseListObservable } from 'angularfire2/database';
+
 
 @Component({
   selector: 'app-select',
   templateUrl: './select.component.html',
-  styleUrls: ['./select.component.scss']
+  styleUrls: ['./select.component.scss'],
+  providers: [GameService]
 })
 export class SelectComponent implements OnInit {
 
-  constructor() { }
+  games: FirebaseListObservable<any[]>;
+
+
+  constructor(private gameService: GameService) { }
 
   ngOnInit() {
+    this.games = this.gameService.getGames();
   }
 
 }
