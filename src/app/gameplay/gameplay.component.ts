f4a5b3ddd198ed3e4a6bd97e45bf956488204d22
jf <julianflood@gmail.com>
2017-09-01 12:23:05 -0700

    get current player info to display on gameplay page, work on getting info to append to player area

--- src/app/gameplay/gameplay.component.ts
+++ src/app/gameplay/gameplay.component.ts
@@ -4,28 +4,60 @@ import { Location } from '@angular/common';
 import { Game } from '../game.model';
 import { GameService } from '../game.service';
 import { FirebaseListObservable } from 'angularfire2/database';
+import { AuthenticationService } from '../authentication.service';
+import { FirebaseToAppService } from '../firebase-app.service';
+
 
 @Component({
   selector: 'app-gameplay',
   templateUrl: './gameplay.component.html',
   styleUrls: ['./gameplay.component.scss'],
-  providers:[GameService]
+  providers:[GameService, AuthenticationService, FirebaseToAppService]
 })
+
 export class GameplayComponent implements OnInit {
 
   gameId:string;
   gameToDisplay;
 
+  profiles;
+  profilesFromDB;
+  currentUserUID;
+  currentUser = null;
+
   constructor(private route: ActivatedRoute,
               private location: Location,
-              private gameService: GameService
-            ) { }
+              private gameService: GameService,
+              private authService: AuthenticationService,
+              private fireService: FirebaseToAppService) {
+
+            this.fireService.getProfiles().subscribe(data => {
+              this.profilesFromDB = data
+               this.currentUserUID = this.authService.afAuth.auth.currentUser.uid
+
+               this.profiles = this.profilesFromDB;
+                 if(this.profiles.length > 0) {
+                   this.profiles.forEach(user => {
+                     if (user.uid === this.currentUserUID) {
+                       this.currentUser = user;
+                       console.log(this.currentUser)
+                     }
+                   })
+                 }
+
+             })
+            }
 
   ngOnInit() {
+    this.profiles = this.profilesFromDB;
     this.route.params.forEach((urlParameters) => {
       this.gameId = urlParameters['id'];
     });
     this.gameToDisplay = this.gameService.getGameById(this.gameId);
+  }
+
+  joinGame(){
+    
   }
 
 }
